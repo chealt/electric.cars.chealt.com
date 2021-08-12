1b2748de36bbca8e39145bb428767cf0f765d6fc
Attila Bartha <Attila.Bartha@ibm.com>
2021-08-12 16:21:17 +0200

    create App component

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-render(<h1>Compare Electric Cars</h1>, document.getElementById('root'));
+import App from './App/App.jsx';
+
+render(<App />, document.getElementById('root'));
